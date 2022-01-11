a65e79d6a23c555e9a2dace23cd2db8c911aaf30
wangailin <wangailin@jd.com>
2022-01-11 20:30:45 +0800

    Merge branch 'dev_detail_v1' of https://gitee.com/jd-platform-opensource/ccms-antd into dev_detail_v1

--- packages/UIs/ccms-antd/src/components/formFields/longtext/display.tsx
+++ packages/UIs/ccms-antd/src/components/formFields/longtext/display.tsx
@@ -7,6 +7,11 @@ export default class LongTextDisplayComponent extends LongTextDisplay {
     const {
       value
     } = props
-    return <React.Fragment>{value}</React.Fragment>
+    const reg = /((ht|f)tp(s?))\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%$#_]*)?/
+    return <div style={{wordBreak: 'break-all'}}>
+      {
+        reg.test(value) ? <a href={value} target="_blank" rel="noreferrer" >{value}</a> : value
+      }
+    </div>
   }
 }
