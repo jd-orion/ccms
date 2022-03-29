7a1deaaa2b6ba766c4e1fa0ec1de2844d49f4764
牛尾巴 <niuxg0@hotmail.com>
2022-03-29 03:19:43 +0000

    !16 feat：detail增加详情说明 Merge pull request !16 from 庞招群/add-detail-explain

--- packages/UIs/ccms-antd/src/components/detail/index.tsx
+++ packages/UIs/ccms-antd/src/components/detail/index.tsx
@@ -3,6 +3,7 @@ import GroupField from './group'
 import StatementDetail from './statement'
 import EnumDetailComponent from './enum'
 import ImportSubformField from './importSubform'
+import InfoDetail from './detailInfo'
 import CustomFieldComponent from './custom'
 
 export default {
@@ -11,5 +12,6 @@ export default {
   statement: StatementDetail,
   detail_enum: EnumDetailComponent,
   import_subform: ImportSubformField,
+  detail_info: InfoDetail,
   custom: CustomFieldComponent
 }
