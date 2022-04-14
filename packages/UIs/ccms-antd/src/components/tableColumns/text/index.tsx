f5fb7277984ad5a8dff34c2e725a54b443b767b9
wangailin <wangailin@jd.com>
2022-04-14 10:15:22 +0800

    feat: 表格多行隐藏

--- packages/UIs/ccms-antd/src/components/tableColumns/text/index.tsx
+++ packages/UIs/ccms-antd/src/components/tableColumns/text/index.tsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import { TextColumn } from 'ccms'
 import { ITextColumn, TextColumnConfig } from 'ccms/dist/src/components/tableColumns/text'
-export const PropsType = (props: TextColumnConfig) => {}
-
+export const PropsType = (props: TextColumnConfig) => { }
+import styles from './index.less'
+import { Modal, Button } from "antd"
 export default class TextColumnComponent extends TextColumn {
-  renderComponent = (props: ITextColumn) => {
+
+  showMore = (props: ITextColumn) => {
     const {
       value
     } = props
 
-    return (<>
-      {value}
-    </>
+    Modal.info({
+      getContainer: () => document.getElementById('ccms-antd') || document.body,
+      content: (<div style={{ overflow: 'hidden' }}>{value}</div>),
+      okText: '确定'
+    });
+  }
+
+  renderComponent = (props: ITextColumn) => {
+    const {
+      value,
+      linkUrl,
+      showLines,
+      showMore
+    } = props
+
+    return (linkUrl ?
+      <a href={value} target='_blank' rel="noreferrer">{value} </a>
+      : showLines > 1 ?
+        <>
+          <div className={styles['ccms-table-text']}>
+            {value}
+          </div>
+          {showMore && <Button onClick={this.showMore} type='link'>查看全部</Button>}
+        </>
+        : <>
+          {value}
+        </>
     )
   }
 }
