1a36d5cd966903948b4e799f4ce73709283df3d9
niuxiaoguang <niuxiaoguang@jd.com>
2022-04-24 17:36:38 +0800

    Merge request #14 from CCMS:dev-rollup
    feat: 工程化改造

--- packages/UIs/ccms-antd-mini/.fatherrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  entry: 'src/index.tsx',
-  cjs: "rollup",
-  cssModules: true,
-  injectCSS: true,
-  doc: {
-    typescript: true
-  }
-}
---
+++ packages/UIs/ccms-antd-mini/rollup.config.js
@@ -0,0 +1,54 @@
+import path from 'path'
+import ts from 'rollup-plugin-typescript2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+import { babel } from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import { eslint } from 'rollup-plugin-eslint'
+import json from '@rollup/plugin-json'
+import postcss from 'rollup-plugin-postcss'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      format: 'cjs',
+      file: path.resolve('dist/index.js')
+    }
+  ],
+  onwarn(warning) {
+    if (warning.code === 'THIS_IS_UNDEFINED') {
+      return
+    }
+    console.error(warning.message)
+  },
+  plugins: [
+    // eslint({
+    //     throwOnError: true,
+    //     exclude: ['node_modules/**', 'lib/**', 'dist/**']
+    // }),
+    postcss({
+      extensions: ['.css', '.less'],
+      modules: true // 启用CSS模块
+    }),
+    json(),
+    ts({
+      tsconfig: path.resolve(__dirname, 'tsconfig.json')
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      plugins: ['@babel/plugin-transform-runtime']
+    }),
+    commonjs(),
+    nodeResolve({
+      extensions: ['.js', '.ts', '.tsx']
+    }),
+    terser()
+  ],
+  external: ['react', 'react-dom', 'ccms', '@ant-design/icons', 'react-color', 'antd', 'moment'],
+  watch: {
+    include: 'src/**'
+  }
+}
