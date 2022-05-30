6185665b76582fdbfa94d396ac538b41e6dfa4d8
wangailin <wangailin@jd.com>
2022-05-30 18:17:21 +0800

    feat: 新增详情页图片预览

--- packages/UIs/ccms-antd/src/components/detail/image/index.tsx
+++ packages/UIs/ccms-antd/src/components/detail/image/index.tsx
@@ -1,20 +1,35 @@
-
 import React from 'react'
 import { DetailImageField } from 'ccms'
 import { IImageDetail, ImageDetailConfig } from 'ccms/dist/src/components/detail/image'
-import { Image } from 'antd'
-export const PropsType = (props: ImageDetailConfig) => { }
+import { EyeOutlined } from '@ant-design/icons'
+import { Space, Tooltip } from 'antd'
+import styles from './index.less'
+
+export const PropsType = (props: ImageDetailConfig) => {}
 
 export default class ImageDetailComponent extends DetailImageField {
   renderComponent = (props: IImageDetail) => {
-    const { value, width, height } = props
-    return (
-      value ?
-        <Image
-          width={width}
-          height={height}
-          src={value}
-        /> : <></>
+    const { value, width, height, preview } = props
+    return value ? (
+      <div className={styles['ccms-antd-detail-image']}>
+        <img className={styles.image} height={height} width={width} src={value} alt={value} />
+        {preview && (
+          <div className={styles.mask}>
+            <Space>
+              <Tooltip getPopupContainer={(ele) => ele.parentElement || document.body} overlay="查看">
+                <EyeOutlined
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    window.open(value)
+                  }}
+                />
+              </Tooltip>
+            </Space>
+          </div>
+        )}
+      </div>
+    ) : (
+      <> </>
     )
   }
 }
