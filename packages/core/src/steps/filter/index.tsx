bbafce2e8fc2f03d0f59767c967e6179c4988076
niuxiaoguang <niuxiaoguang@jd.com>
2022-08-09 14:36:43 +0800

    fix: 解决编译问题

--- packages/core/src/steps/filter/index.tsx
+++ packages/core/src/steps/filter/index.tsx
@@ -119,6 +119,7 @@ export default class FilterStep extends Step<FilterConfig, FilterState> {
 
     if (this.props.config.defaultValue) {
       const formDefault = ParamHelper(this.props.config.defaultValue, {
+        record: {},
         data: this.props.data,
         step: this.props.step,
         containerPath: ''
@@ -228,6 +229,7 @@ export default class FilterStep extends Step<FilterConfig, FilterState> {
 
     if (this.props.config.defaultValue) {
       const formDefault = ParamHelper(this.props.config.defaultValue, {
+        record: {},
         data: this.props.data,
         step: this.props.step,
         containerPath: ''
