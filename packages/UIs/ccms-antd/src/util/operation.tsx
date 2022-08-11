f5bb888438aa55298bddcf64b8bc70fb3135af33
niuxiaoguang <niuxiaoguang@jd.com>
2022-08-11 17:09:39 +0800

    fix: 修复OperationHelper组件UI展示失败的问题

--- packages/UIs/ccms-antd/src/util/operation.tsx
+++ packages/UIs/ccms-antd/src/util/operation.tsx
@@ -1,16 +1,14 @@
-import React from "react"
-import { OperationHelper as _OperationHelper } from "ccms"
-import { Modal } from "antd"
-import CCMS from "../main"
-import { CCMSProps } from "ccms/dist/src/main"
-import { IOperationModal } from "ccms/dist/src/util/operation"
+import React from 'react'
+import { OperationHelper as _OperationHelper } from 'ccms'
+import { Modal } from 'antd'
+import CCMS from '../main'
 
 export default class OperationHelper extends _OperationHelper {
-  protected renderModal (props: IOperationModal) {
+  protected renderModal = (props) => {
     return (
       <Modal
-        visible={true}
-        forceRender={true}
+        visible
+        forceRender
         title={props.title}
         footer={null}
         getContainer={() => document.getElementById('ccms-antd') || document.body}
@@ -21,7 +19,7 @@ export default class OperationHelper extends _OperationHelper {
     )
   }
 
-  protected renderCCMS (props: CCMSProps) {
+  protected renderCCMS = (props) => {
     return <CCMS {...props} />
   }
 }
