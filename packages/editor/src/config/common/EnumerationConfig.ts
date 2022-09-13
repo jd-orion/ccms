f048cdde7f3ac958ad24b366203412db0b227a27
niuxg0 <niuxg0@hotmail.com>
2022-09-13 16:18:34 +0800

    feat: 补充配置项

--- packages/editor/src/config/common/EnumerationConfig.ts
+++ packages/editor/src/config/common/EnumerationConfig.ts
@@ -64,6 +64,34 @@ const config: FieldConfigs[] = [
             type: 'any'
           }
         ]
+      },
+      {
+        field: 'disabled',
+        label: '禁用条件',
+        type: 'import_subform',
+        interface: {
+          url: '${configDomain}/common/ConditionConfig.json',
+          urlParams: [
+            {
+              field: 'version',
+              data: {
+                source: 'source',
+                field: 'version'
+              }
+            },
+            {
+              field: 'configDomain',
+              data: {
+                source: 'source',
+                field: 'configDomain'
+              }
+            }
+          ],
+          method: 'GET',
+          cache: {
+            global: 'CCMS_CONFIG_common_ConditionConfig'
+          }
+        }
       }
     ],
     condition: {
