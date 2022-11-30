13b0f74c2d8d70804b1cc8ce0655e4709ba9fbd2
wangailin <wangailin@jd.com>
2022-11-30 10:54:26 +0800

    feat: 更新表格跳转逻辑

--- packages/UIs/ccms-antd/src/components/tableColumns/text/index.tsx
+++ packages/UIs/ccms-antd/src/components/tableColumns/text/index.tsx
@@ -1,38 +1,58 @@
 import React from 'react'
 import { TextColumn } from 'ccms'
 import { ITextColumn, TextColumnConfig } from 'ccms/dist/src/components/tableColumns/text'
-export const PropsType = (props: TextColumnConfig) => { }
+import { Modal, Button } from 'antd'
 import styles from './index.less'
-import { Modal, Button } from "antd"
-export default class TextColumnComponent extends TextColumn {
 
+export const PropsType = (props: TextColumnConfig) => { }
+
+export default class TextColumnComponent extends TextColumn {
   showMore = (value) => {
     Modal.info({
       getContainer: () => document.getElementById('ccms-antd') || document.body,
-      content: (<div style={{ overflow: 'auto', maxHeight: '400px' }}>{value}</div>),
+      content: <div style={{ overflow: 'auto', maxHeight: '400px' }}>{value}</div>,
       okText: '确定',
       width: '50%',
       maskClosable: true
-    });
+    })
+  }
+
+  link = (value) => {
+    window.open(value, '_blank')
   }
 
   renderComponent = (props: ITextColumn) => {
-    const {
-      value,
-      linkUrl,
-      showLines,
-      showMore
-    } = props
+    const { value, linkUrl, showLines, showMore } = props
 
-    return (showLines && showLines > 0 ?
+    return showLines && showLines > 0 ? (
       <>
-        {showMore && <Button className={styles['ccms-table-text-button']} onClick={() => this.showMore(value)} type='link'>查看</Button>}
-        <div className={styles['ccms-table-text']} style={{ WebkitBoxOrient: 'vertical', WebkitLineClamp: showLines || 0, lineClamp: showLines || 0 }}>
-          {linkUrl ? <a href={value} target='_blank' rel="noreferrer">{value} </a> :  value}
+        {showMore && (
+          <Button className={styles['ccms-table-text-button']} onClick={() => this.showMore(value)} type="link">
+            查看
+          </Button>
+        )}
+        <div
+          className={styles['ccms-table-text']}
+          style={{ WebkitBoxOrient: 'vertical', WebkitLineClamp: showLines || 0, lineClamp: showLines || 0 }}
+        >
+          {linkUrl ? (
+            <div className={styles['ccms-table-text-link']} onClick={() => this.link(value)} aria-hidden="true">
+              {value}{' '}
+            </div>
+          ) : (
+            value
+          )}
         </div>
       </>
-      : <>
-        {linkUrl ? <a href={value} target='_blank' rel="noreferrer">{value} </a> :  value } 
+    ) : (
+      <>
+        {linkUrl ? (
+          <div className={styles['ccms-table-text-link']} onClick={() => this.link(value)} aria-hidden="true">
+            {value}{' '}
+          </div>
+        ) : (
+          value
+        )}
       </>
     )
   }
